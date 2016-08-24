effd95de00d073c8b294ef3ecceefa9cd92f02da
Trevor Overman <troverman@gmail.com>
2016-08-24 18:35:10 -0400

    YAY
    Former-commit-id: 57e2753a2c13a6c8bc7d50294cd33e8aca50372b

--- assets/src/app/connect/index.js
+++ assets/src/app/connect/index.js
@@ -1,7 +1,7 @@
 angular.module( 'conexus.connect', [
 ])
 
-.config(function config( $stateProvider ) {
+.config(function config( $stateProvider, uiGmapGoogleMapApiProvider) {
 	$stateProvider.state( 'connect', {
 		url: '/connect',
 		views: {
@@ -11,18 +11,12 @@ angular.module( 'conexus.connect', [
 			}
 		}
 	});
-},
-
-function(uiGmapGoogleMapApiProvider) {
-    uiGmapGoogleMapApiProvider.configure({
-        //key: 'AIzaSyDKPi-Krk_GCd_YfHS4ghUH3P4afPtPstA',
-		key: 'AIzaSyBfn7VlFh9rYu_RQHIc3eUtcrrrm1PkObQ',
-        v: '3.17',
-        libraries: 'weather,geometry'
+	uiGmapGoogleMapApiProvider.configure({
+        key: 'AIzaSyDKPi-Krk_GCd_YfHS4ghUH3P4afPtPstA',
+        v: '3.20',
+        libraries: 'weather,geometry,visualization'
     });
-}
-
-)
+})
 
 .controller( 'ConnectCtrl', function ConnectController( $scope, titleService, UserModel, uiGmapGoogleMapApi ) {
 	titleService.setTitle('connect - conex.us');
