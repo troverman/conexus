1f77797a07f6feeec1a71596e0e371566176adb1
Trevor Overman <troverman@gmail.com>
2016-08-24 17:38:03 -0400

    keYYY:
    Former-commit-id: 86f3561fc3d2f7c010ef2c42a03b4d72b5cea427

--- assets/src/app/connect/index.js
+++ assets/src/app/connect/index.js
@@ -15,7 +15,7 @@ angular.module( 'conexus.connect', [
 
 function(uiGmapGoogleMapApiProvider) {
     uiGmapGoogleMapApiProvider.configure({
-        key: 'AIzaSyDiHDaKzLj54Fc3gsI4Fd3KPKpWAJCkgO0',
+        key: 'AIzaSyBfn7VlFh9rYu_RQHIc3eUtcrrrm1PkObQ',
         v: '3.17',
         libraries: 'weather,geometry'
     });
