cb9a4fe3a3744e0eead11526eb9c465eb45f2e3a
Trevor Overman <troverman@gmail.com>
2016-10-07 02:25:12 -0400

    index
    Former-commit-id: 54418abfc50450031bd056ddac9fbcb3b37cb31a

---
+++ api/models/UserLocation.js
@@ -0,0 +1,73 @@
+module.exports = {
+    attributes: {
+        title: {
+            type: 'string',
+            required: true
+        },
+        project: {
+            model: 'project',
+            required: true
+        },
+        user: {
+            model: 'user',
+            required: true
+        }
+    },
+
+    /**
+    * Callback to be run after creating a Message.
+    *
+    * @param {Object}   message The soon-to-be-created Message
+    * @param {Function} next
+    */
+    afterCreate: function (message, next) {
+        // set message.user = to appropriate user model
+        User.getOne(message.user)
+        .spread(function(user) {
+            message.user = user;
+            next(null, message);
+        });
+    },
+
+    getByProject: function(project) {
+        return Message.find()
+        .where({project: project})
+        .sort({createdAt: 'desc'})
+        .populate('user')
+        .populate('project')
+        .then(function (models) {
+            return [models];
+        });
+    },
+
+    getByUser: function(user) {
+        return Message.find()
+        .where({user: user})
+        .sort({createdAt: 'desc'})
+        .populate('user')
+        .populate('project')
+        .then(function (models) {
+            return [models];
+        });
+    },
+
+    getAll: function() {
+        return Message.find()
+        .sort({createdAt: 'desc'})
+        .populate('user')
+        .populate('project')
+        .then(function (models) {
+            return [models];
+        });
+    },
+
+    getOne: function(id) {
+        return Message.findOne(id)
+        .populate('user')
+        .populate('project')
+        .then(function (model) {
+            // you have the option to do something with the model here if needed, before returning it to the controller
+            return [model];
+        });
+    }
+};
