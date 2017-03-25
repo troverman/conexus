c66c40251d4facbed3dcb31eabced4ba7e76cb78
Trevor Overman <troverman@gmail.com>
2017-03-25 19:52:24 -0400

    s
    Former-commit-id: fcb712a99beea1340a4980784b2d59c703963401

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -5,6 +5,7 @@ module.exports = {
             currentUser: req.user
         });
     },
+    
     ssl: function(req, res) {
     	res.send('cV2kFOb-PA_snEp6iR9udCIa1XFiJ8tm3JZkYOouqmw.yMz-EAV5agQah1zn-w6Aqp0JVzxv1jmSFH6dh5Ea9uI')
     },
