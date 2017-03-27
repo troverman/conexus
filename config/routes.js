7bcd4dc5430b26cadaf8b678f25325df8da02b71
Trevor Overman <troverman@gmail.com>
2017-03-27 02:48:17 -0400

    fix
    Former-commit-id: 76d3cf338578ea8908ea8db29df4de1db17bc505

--- config/routes.js
+++ config/routes.js
@@ -2,39 +2,11 @@
  * Route Mappings
  * (sails.config.routes)
  *
- * Your routes map URLs to views and controllers.
- *
- * If Sails receives a URL that doesn't match any of the routes below,
- * it will check for matching files (images, scripts, stylesheets, etc.)
- * in your assets directory.  e.g. `http://localhost:1337/images/foo.jpg`
- * might match an image file: `/assets/images/foo.jpg`
- *
- * Finally, if those don't match either, the default 404 handler is triggered.
- * See `api/responses/notFound.js` to adjust your app's 404 logic.
- *
- * Note: Sails doesn't ACTUALLY serve stuff from `assets`-- the default Gruntfile in Sails copies
- * flat files from `assets` to `.tmp/public`.  This allows you to do things like compile LESS or
- * CoffeeScript for the front-end.
- *
- * For more information on configuring custom routes, check out:
- * http://sailsjs.org/#/documentation/concepts/Routes/RouteTargetSyntax.html
  */
 
 module.exports.routes = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Custom routes here...                                                    *
-  *                                                                          *
-  *  If a request to a URL doesn't match any of the custom routes above, it  *
-  * is matched against Sails route blueprints. See `config/blueprints.js`    *
-  * for configuration options and examples.                                  *
-  *                                                                          *
-  ***************************************************************************/
-
-
   'get /': 'HomeController.index',
-
   'get /home': 'HomeController.index',
   'get /about': 'HomeController.index',
   'get /account': 'HomeController.index',
@@ -67,8 +39,6 @@ module.exports.routes = {
   'get /auth/:provider/callback': 'AuthController.callback',
   'get /auth/:provider/:action': 'AuthController.callback',
 
-
-
   /**
    * Follower routes
    *
@@ -79,7 +49,6 @@ module.exports.routes = {
   'get /api/follower/following/:id': 'FollowerController.getFollowing',
   'post /api/follower': 'FollowerController.create',
   'delete /api/follower/:id': 'FollowerController.destroy',
-
 
   /**
    * Message routes
@@ -138,7 +107,6 @@ module.exports.routes = {
    */
   'get /api/search/:searchQuery': 'SearchController.search',
   'get /api/search': 'SearchController.searchAll',
-
 
   /**
    * Task routes
