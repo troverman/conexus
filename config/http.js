2fa8fb7beab60292a1f874fa051a360b239542e7
Trevor Overman <troverman@gmail.com>
2017-09-29 20:01:20 -0400

    SEO
    Former-commit-id: 9699b09d2306b7391172aba4538aca976e816da7

--- config/http.js
+++ config/http.js
@@ -11,6 +11,35 @@
 
 module.exports.http = {
 
+
+  middleware: {
+
+     prerender: require('prerender-node').set('prerenderServiceUrl', 'https://tranquil-reef-73037.herokuapp.com/').set('prerenderToken', 'V8W4l4iLL7BRD4pB8stg'),
+     //prerender: require('prerender-node').set('prerenderToken', 'V8W4l4iLL7BRD4pB8stg'),
+     order: [
+       'startRequestTimer',
+       'cookieParser',
+       'session',
+       'myRequestLogger',
+       'bodyParser',
+       'handleBodyParserError',
+       'prerender',
+       'compress',
+       'methodOverride',
+       'poweredBy',
+       '$custom',
+       'router',
+       'www',
+       'favicon',
+       '404',
+       '500'
+     ],
+     //myRequestLogger: function (req, res, next) {
+           //console.log("Requested :: ", req.method, req.url);
+           //return next();
+     //}
+  }
+
   /****************************************************************************
   *                                                                           *
   * Express middleware to use for every Sails request. To add custom          *
